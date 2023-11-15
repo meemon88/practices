d9ac3648227923807543378c209d811275dbb294
khair <meemon88@gmail.com>
2023-11-15 19:45:51 +0900

    update

--- lib/string.dart
+++ lib/string.dart
@@ -1,3 +1,12 @@
+// void main() {
+//   String baseURL = "https://www.youtube.com/watch?v=k0EBQioLCR0";
+//   if(baseURL.contains("=")){
+//     print(baseURL.split('=').last);
+//   }else{
+//     print('Not Found "=" equal in your link');
+//   }
+// }
+
 void main() {
   String baseURL = "https://www.youtube.com/watch?v=k0EBQioLCR0";
   if(baseURL.contains("=")){
